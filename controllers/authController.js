3db2613a190bb01e8432877621ef2f54767e800c
Kaustuv Karki <karkikaustuv@gmail.com>
2023-06-08 23:30:19 +0545

    starting with jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
 import { hashPassword } from "../helpers/authHelper.js";
 import userModel from "../models/userModel.js";
+import JWT from "jsonwebtoken";
+
 export const registerController = async (req, res) => {
     try {
         const { name, email, password, phone, address } = req.body;
@@ -50,6 +52,20 @@ export const registerController = async (req, res) => {
         res.status(500).send({
             success: false,
             message: "Error in registration",
+            error,
+        });
+    }
+};
+
+// POST LOGIN
+
+export const loginController = async (req, res) => {
+    try {
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({
+            success: false,
+            message: "Error in Login",
             error,
         });
     }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { registerController } from "../controllers/authController.js";
+import {
+    loginController,
+    registerController,
+} from "../controllers/authController.js";
 
 //router
 
 const router = express.Router();
 
+// ReGISTER
 router.post("/register", registerController);
+
+// LOGIN
+router.post("/login", loginController);
 
 export default router;

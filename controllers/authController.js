b597ca5d8bbaf7dc3a622a9c7efcb6c697840bd0
Kaustuv Karki <karkikaustuv@gmail.com>
2023-06-09 18:00:21 +0545

    login not working have to work

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-import { hashPassword } from "../helpers/authHelper.js";
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
 import userModel from "../models/userModel.js";
 import JWT from "jsonwebtoken";
 
@@ -61,11 +61,52 @@ export const registerController = async (req, res) => {
 
 export const loginController = async (req, res) => {
     try {
+        const { email, password } = req.body;
+
+        // validation
+        if (!email || !password) {
+            return res.status(404).send({
+                success: false,
+                message: "Invalid Email or Password",
+            });
+        }
+
+        //check user
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res
+                .status(404)
+                .send({ success: false, message: "User not found" });
+        }
+        const match = await comparePassword(password, user.password);
+        if (!match) {
+            return res
+                .staus(200)
+                .send({ success: false, message: "Invalid password" });
+        }
+
+        // token
+        const token = await JWT.sign(
+            { _id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "7d" }
+        );
+
+        res.status(200).send({
+            success: true,
+            message: "Login Successful",
+            user: {
+                name: user.name,
+                email: user.email,
+                address: user.address,
+            },
+            token,
+        });
     } catch (error) {
         console.log(error.message);
         res.status(500).send({
             success: false,
-            message: "Error in Login",
+            message: "Login Error",
             error,
         });
     }

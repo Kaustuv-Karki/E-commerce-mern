0587db22379f46365d3ea13ef3d30bb14213953c
Kaustuv Karki <karkikaustuv@gmail.com>
2023-06-17 12:26:59 +0545

    navbar created

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <>
+            <nav
+                className="navbar navbar-expand-lg bg-body-tertiary"
+                data-bs-theme="dark">
+                <div className="container-fluid">
+                    <NavLink to="/" className="navbar-brand">
+                        Navbar
+                    </NavLink>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarTogglerDemo02"
+                        aria-controls="navbarTogglerDemo02"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div
+                        className="collapse navbar-collapse"
+                        id="navbarTogglerDemo02">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink
+                                    to="/"
+                                    className="nav-link active"
+                                    aria-current="page"
+                                    href="#">
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/" className="nav-link" href="#">
+                                    Link
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
 };
 
 export default Header;

17f6d2e179d86cd6c2835dcc299391ea446dac68
Kaustuv Karki <karkikaustuv@gmail.com>
2023-06-27 22:57:22 +0545

    register page

--- client/src/pages/Auth/Register.jsx
+++ client/src/pages/Auth/Register.jsx
@@ -1,25 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../../components/Layout/Layout";
 
 const Register = () => {
+    const [userName, setuserName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [address, setAddress] = useState("");
+    const [phone, setPhone] = useState("");
+
     return (
         <Layout title="Register Yourself">
-            <div className="register">
-                <h1>Register Page</h1>
-                <form>
+            <div className="register" style={{ paddingTop: "-2rem" }}>
+                <h1 style={{ fontSize: "2.5rem", marginBottom: "4rem" }}>
+                    Register Page
+                </h1>
+                <form style={{ minWidth: "20rem" }} className="register-form">
                     <div className="form-group">
-                        <label htmlFor="exampleInputName">Username</label>
                         <input
                             type="text"
+                            value={userName}
                             className="form-control"
                             id="exampleInputName"
-                            placeholder="Enter username"
+                            placeholder="Username"
+                            onChange={(e) => setuserName(e.target.value)}
                         />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="exampleInputPassword1">Password</label>
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => {
+                                setEmail(e.target.value);
+                            }}
+                            className="form-control"
+                            id="exampleInputEmail"
+                            placeholder="Email"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            value={phone}
+                            onChange={(e) => {
+                                setPhone(e.target.value);
+                            }}
+                            className="form-control"
+                            id="exampleInputName"
+                            placeholder="Phone Number"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            value={address}
+                            onChange={(e) => {
+                                setAddress(e.target.value);
+                            }}
+                            className="form-control"
+                            id="exampleInputAddress"
+                            placeholder="Address"
+                        />
+                    </div>
+                    <div className="form-group">
                         <input
                             type="password"
+                            value={password}
+                            onChange={(e) => {
+                                setPassword(e.target.value);
+                            }}
                             className="form-control"
                             id="exampleInputPassword1"
                             placeholder="Password"
